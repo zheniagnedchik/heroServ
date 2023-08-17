67c904c081945cd8d23a4a7c00c167615c6a1a69
zheniagnedchik <46835298+zheniagnedchik@users.noreply.github.com>
2023-08-17 16:39:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const path = require("path");
 
 const app = express();
 app.use("/thumbnails", express.static(path.join(__dirname, "thumbnails")));
+app.use("/avatar", express.static(path.join(__dirname, "avatar")));
 const port = 3000;
 app.use(express.json());
 
