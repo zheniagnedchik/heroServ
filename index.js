442e8730a137ca0a786919212a0c86a4f24edaae
zheniagnedchik <46835298+zheniagnedchik@users.noreply.github.com>
2023-08-18 06:59:37 +0300

    add

--- index.js
+++ index.js
@@ -203,7 +203,7 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 app.post("/upload", upload.single("image"), async (req, res) => {
-  console.log(req)
+  console.log(req);
   try {
     const { email, folder } = req.body; // Email пользователя
     const user = await findUserByEmail(email);
