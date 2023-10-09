4581ad2f21caf84362e56c183c3b0bbf2183c0f4
zheniagnedchik <46835298+zheniagnedchik@users.noreply.github.com>
2023-10-09 07:19:05 +0300

    add

--- controllers/userController.js
+++ controllers/userController.js
@@ -580,7 +580,7 @@ exports.addEat = async (req, res) => {
     if (!user) {
       return res.status(404).json({ message: "Пользователь не найден" });
     }
-    user.eat = [...userId.eat, ...eat];
+    user.eat = [...user.eat, ...eat];
     await session.saveChanges();
     return res.json({ message: "Объект добавлен в поле events пользователя." });
   } catch (error) {
