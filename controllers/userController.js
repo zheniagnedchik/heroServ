256e4a8a3abb02700c3d5c6e4c48adc9c2e05d4c
zheniagnedchik <46835298+zheniagnedchik@users.noreply.github.com>
2023-11-24 13:52:47 +0300

    add

--- controllers/userController.js
+++ controllers/userController.js
@@ -790,7 +790,7 @@ exports.generatePost = async (req, res) => {
     };
 
     const data = {
-      model: "ada",
+      model: "gpt-3.5-turbo",
       messages: [
         {
           role: "user",
