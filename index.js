e15f4b2c9b9555b6ca161e24b292f359d12eda52
zheniagnedchik <46835298+zheniagnedchik@users.noreply.github.com>
2024-01-17 05:05:15 -0800

    add

--- index.js
+++ index.js
@@ -114,6 +114,7 @@ app.post("/add_chat_folder",chatFoldersControler.addChatFolder);
 app.post("/get_chat_folder",chatFoldersControler.getChatFolders);
 app.post("/add_folder_to_dialog",dialogController.addFolderToDialog);
 app.post("/del_chat_folder",chatFoldersControler.delChatFolder);
+app.post("/del_chat", dialogController.delChat);
 app.get("/convert", (req, res) => {
   const inputPath = path.join(__dirname, "1698739988758.mp4");
   const outputPath = path.join(__dirname, "output_video.mp4");
