306531ae075fb6c8c89d33adaf81394f3a47edd5
zheniagnedchik <46835298+zheniagnedchik@users.noreply.github.com>
2024-01-24 07:47:33 -0800

    add

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ app.post("/del_chat", dialogController.delChat);
 app.post("/del_service", userController.deleteService);
 app.post("/get_hash_tag", hashTagController.getHashTags);
 app.post("/add_hash_tag", hashTagController.addHAshTag);
-app.post("get_clients", hashTagController.getClients);
+app.post("/get_clients", hashTagController.getClients);
 app.get("/convert", (req, res) => {
   const inputPath = path.join(__dirname, "1698739988758.mp4");
   const outputPath = path.join(__dirname, "output_video.mp4");
